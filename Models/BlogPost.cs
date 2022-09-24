532184fc714c36ffdf7fb625819526472ef9622a
BozhidarA <bplatanasov@gmail.com>
2022-09-24 22:06:12 +0300

    Made create post look better

--- Models/BlogPost.cs
+++ Models/BlogPost.cs
@@ -7,13 +7,16 @@ namespace ASPLiteBlog.Models
     public class BlogPost
     {
         public int ID { get; set; }
+        [Display(Name = "Title:")]
         public string title { get; set; }
+        [Display(Name = "Body:")]
         public string body { get; set; }
         public string previewPicLocation { get; set; }
         public DateTime timeCreated { get; set; } = DateTime.UtcNow;
         public DateTime? timeLastEdited { get; set; }
         public DateTime? timePublished { get; set; }
         public DateTime? timeLastEditedPublished { get; set; }
+        [Display(Name = "Is this a draft?")]
         public bool draft { get; set; }
 
         public string userID { get; set; }
